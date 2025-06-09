cb8c0e4591ddd921cb07f5bfc34915167e6defa0
Matheus Melo <matheusmelo@hotmail.com.br>
2025-06-09 17:37:59 -0300

    adicionada a condição de IF para futura alteração do IF

--- js/main.js
+++ js/main.js
@@ -11,14 +11,18 @@ formulario.addEventListener("submit", enviarFormulario);
 async function enviarFormulario(event) {
   event.preventDefault();
 
-  // const id = document.getElementById("id").value;
+  const id = document.getElementById("id").value;
   const especie = document.getElementById("form_especie").value;
   const nome = document.getElementById("form_nome").value;
   const raca = document.getElementById("form_raca").value;
 
   try {
-    await api.cadastrarPet({ especie, nome, raca });
-    ui.mostrarPets();
+    if (id) {
+      await api.alterarInformacoesPorId(id);
+    } else {
+      await api.cadastrarPet({ especie, nome, raca });
+      ui.mostrarPets();
+    }
   } catch (error) {
     alert(`Um erro ocorreu ao cadastrar o pet: ${error}`);
   }
