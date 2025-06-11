bf387f97a397f28150cda5bbb18189d5ef1b7038
Matheus Melo <matheusmelo@hotmail.com.br>
2025-06-11 19:18:56 -0300

    alterado para usar o axios ao invés das ferramentas nativas do javascript

--- js/api.js
+++ js/api.js
@@ -1,8 +1,10 @@
+const URL_BASE = "http://localhost:3001/";
+
 const api = {
   async buscarInformacoesDePet() {
     try {
-      const response = await fetch("http://localhost:3001/pets");
-      return await response.json();
+      const response = await axios.get(`${URL_BASE}pets`);
+      return await response.data;
     } catch {
       alert("Ocorreu um erro ao buscar as informações dos PETS.");
       throw error;
@@ -11,12 +13,8 @@ const api = {
 
   async cadastrarPet(pet) {
     try {
-      const response = await fetch("http://localhost:3001/pets", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(pet),
-      });
-      return response.json();
+      const response = await axios.post(`${URL_BASE}pets`, pet);
+      return response.data;
     } catch (error) {
       alert(`Erro ao cadastrar um novo pet: ${error}`);
     }
@@ -24,8 +22,8 @@ const api = {
   // Busca Pet Por ID (verificar se estou fazendo a coisa certa com base no metodo PUT do exercício)
   async buscarPetPorId(id) {
     try {
-      const response = await fetch(`http://localhost:3001/pets/${id}`);
-      return await response.json();
+      const response = await axios.get(`${URL_BASE}pets/${id}`);
+      return await response.data;
     } catch (error) {
       alert(`Não foi possível buscar por esse pet, ${error}`);
     }
@@ -33,14 +31,19 @@ const api = {
   // Altera as informações do PET baseadas no ID fornecido
   async alterarInformacoesPorId(pet) {
     try {
-      const response = await fetch(`http://localhost:3001/${pet.id}`, {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(pet),
-      });
-      return response.json();
+      const response = await axios.put(`${URL_BASE}pets/${pet.id}`, pet);
+      return response.data;
     } catch (error) {
       alert(`Não foi possível alterar esse pet: ${error}`);
+    }
+  },
+
+  //Apagar informações dos pets
+  async excluirPet(id) {
+    try {
+      const response = await axios.delete(`${URL_BASE}pets/${id}`);
+    } catch (error) {
+      alert(`Erro ao apagar pet: ${error}`);
     }
   },
 };
